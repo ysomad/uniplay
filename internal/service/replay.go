4473e8fa5c7bcc5c93ea3562e9cfe1fcf68fc1d1
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-11-13 17:22:35 +0700

    removed unes comment

--- internal/service/replay.go
+++ internal/service/replay.go
@@ -27,11 +27,6 @@ func (r *replay) CollectStats(ctx context.Context, replay io.Reader) (*domain.Ma
 		return nil, err
 	}
 
-	/*
-		1. сделать match id из мета информации из демки, например "карта-длительность-команда1-счет1-команда2-счет2" в base64
-		2. создать матч, если создался, то создавать стату, если нет - вернуть ошибку что эта демка уже была загружена ранее.
-	*/
-
 	a := res.CreateMatchArgs()
 	m, err := domain.NewMatch(a.MapName, a.Duration, a.Team1, a.Team2)
 	if err != nil {
