bbf11f064e3a2d44004af9370904e4fdbe018d6c
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-11-17 23:18:51 +0700

    added dry run make cmd

--- internal/app/app.go
+++ internal/app/app.go
@@ -47,44 +47,44 @@ func Run(conf *config.Config) {
 	replayService := service.NewReplay(l, replayRepo)
 
 	// test all
-	replayFiles, err := os.ReadDir("./test-data/")
-	if err != nil {
-		l.Fatal("os.ReadDir", zap.Error(err))
-	}
+	// replayFiles, err := os.ReadDir("./test-data/")
+	// if err != nil {
+	// 	l.Fatal("os.ReadDir", zap.Error(err))
+	// }
 
-	for _, file := range replayFiles {
-		if file.Name() == ".DS_Store" {
-			continue
-		}
+	// for _, file := range replayFiles {
+	// 	if file.Name() == ".DS_Store" {
+	// 		continue
+	// 	}
 
-		replayFile, err := os.Open("./test-data/" + file.Name())
-		if err != nil {
-			l.Fatal("open file error", zap.Error(err))
-		}
-		defer replayFile.Close()
+	// 	replayFile, err := os.Open("./test-data/" + file.Name())
+	// 	if err != nil {
+	// 		l.Fatal("open file error", zap.Error(err))
+	// 	}
+	// 	defer replayFile.Close()
 
-		err = atomic.Run(context.Background(), func(txCtx context.Context) error {
-			_, err = replayService.CollectStats(txCtx, replayFile)
-			return err
-		})
-		if err != nil {
-			l.Fatal("demo collect error", zap.Error(err))
-		}
-	}
+	// 	err = atomic.Run(context.Background(), func(txCtx context.Context) error {
+	// 		_, err = replayService.CollectStats(txCtx, replayFile)
+	// 		return err
+	// 	})
+	// 	if err != nil {
+	// 		l.Fatal("demo collect error", zap.Error(err), zap.String("filename", file.Name()))
+	// 	}
+	// }
 
 	// test one
-	// replayFile, err := os.Open("./test-data/5.dem")
-	// if err != nil {
-	// 	l.Fatal("open file error", zap.Error(err))
-	// }
-	// defer replayFile.Close()
+	replayFile, err := os.Open("./test-data/quazar_11.dem")
+	if err != nil {
+		l.Fatal("open file error", zap.Error(err))
+	}
+	defer replayFile.Close()
 
-	// err = atomic.Run(context.Background(), func(txCtx context.Context) error {
-	// 	_, err = replayService.CollectStats(txCtx, replayFile)
-	// 	return err
-	// })
-	// if err != nil {
-	// 	l.Fatal("demo collect error", zap.Error(err))
-	// }
+	err = atomic.Run(context.Background(), func(txCtx context.Context) error {
+		_, err = replayService.CollectStats(txCtx, replayFile)
+		return err
+	})
+	if err != nil {
+		l.Fatal("demo collect error", zap.Error(err))
+	}
 
 }
