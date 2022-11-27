ec7cb803c1f035eb3c1e880687b01687c98fd790
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-11-27 13:53:18 +0700

    ref: removed unec code

--- internal/domain/weapon_class.go
+++ internal/domain/weapon_class.go
@@ -1,20 +1,6 @@
 package domain
 
-type WeaponClassID uint8
-
-const (
-	ClassPistol       WeaponClassID = 1
-	ClassSMG          WeaponClassID = 2
-	ClassShotgun      WeaponClassID = 3
-	ClassMachineGun   WeaponClassID = 4
-	ClassAssaultRifle WeaponClassID = 5
-	ClassSniperRifle  WeaponClassID = 6
-	ClassEquipment    WeaponClassID = 7
-	ClassOther        WeaponClassID = 8
-	ClassGrenade      WeaponClassID = 9
-)
-
 type WeaponClass struct {
-	ID    WeaponClassID `json:"id"`
-	Class string        `json:"class"`
+	ID    uint8  `json:"id"`
+	Class string `json:"class"`
 }
