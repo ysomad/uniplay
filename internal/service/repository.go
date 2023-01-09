4f6af533d4f8464293bfd16c10af75a52166e66b
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2023-01-09 03:41:17 +0300

    ref: removed metric from domain, dto pkg

--- internal/service/repository.go
+++ internal/service/repository.go
@@ -3,16 +3,8 @@ package service
 import (
 	"context"
 
-	"github.com/google/uuid"
-
 	"github.com/ssssargsian/uniplay/internal/domain"
-	"github.com/ssssargsian/uniplay/internal/dto"
 )
-
-type replayRepository interface {
-	SaveStats(context.Context, *dto.ReplayMatch, []dto.PlayerStat, []dto.PlayerWeaponStat) (*domain.Match, error)
-	MatchExists(ctx context.Context, matchID uuid.UUID) (found bool, err error)
-}
 
 type playerRepository interface {
 	GetTotalStats(ctx context.Context, steamID uint64) (*domain.PlayerTotalStats, error)
