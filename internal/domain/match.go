b7c1a5f3f2d4251f13ae13284d22fbfb47046bd8
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2023-01-10 11:36:44 +0300

    ref: match id errors

--- internal/domain/match.go
+++ internal/domain/match.go
@@ -50,27 +50,27 @@ type MatchTeam struct {
 // NewMatchID returns match id generated from meta data received from replay header.
 func NewMatchID(server, client, mapName string, matchDuration time.Duration, ticks, frames, signonLen int) (uuid.UUID, error) {
 	if server == "" {
-		return uuid.UUID{}, errors.New("server name cannot be empty string")
+		return uuid.UUID{}, errors.New("server name cannot be empty")
 	}
 
 	if client == "" {
-		return uuid.UUID{}, errors.New("client name cannot be empty string")
+		return uuid.UUID{}, errors.New("client name cannot be empty")
 	}
 
 	if matchDuration < minMatchDuration {
-		return uuid.UUID{}, fmt.Errorf("match must last more than %d", minMatchDuration)
+		return uuid.UUID{}, fmt.Errorf("match must last more than %s", minMatchDuration.String())
 	}
 
 	if ticks <= 0 {
-		return uuid.UUID{}, errors.New("got invalid amount of playback ticks")
+		return uuid.UUID{}, errors.New("invalid amount of playback ticks")
 	}
 
 	if frames <= 0 {
-		return uuid.UUID{}, errors.New("got invalid amount of playback frames")
+		return uuid.UUID{}, errors.New("invalid amount of playback frames")
 	}
 
 	if signonLen <= 0 {
-		return uuid.UUID{}, errors.New("got invalid amount of signon length")
+		return uuid.UUID{}, errors.New("invalid signon length")
 	}
 
 	s := fmt.Sprintf("%s,%s,%s,%d,%d,%d,%d", server, client, mapName, matchDuration, ticks, frames, signonLen)
