149cf18557d0678e37dc4977357d2c52c04b1973
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2023-12-01 21:10:07 +0400

    feat: upload demo openapi spec

--- server/cmd/demoparser/main.go
+++ server/cmd/demoparser/main.go
@@ -1,22 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"log/slog"
 	"net/http"
 	"os"
 
+	"github.com/google/uuid"
+
 	"github.com/ysomad/uniplay/internal/pkg/demoparser"
 )
 
-func uploadDemoHandlerV1(w http.ResponseWriter, r *http.Request) {
+type httpError struct {
+	Code string `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+func writeHttpError(w http.ResponseWriter, code int, err error) error {
+	w.WriteHeader(code)
+	return json.NewEncoder(w).Encode(httpError{
+		Msg:  err.Error(),
+		Code: http.StatusText(code),
+	})
+}
+
+type uploadDemoRes struct {
+	DemoJobID uuid.UUID `json:"demo_job_id"`
+}
+
+type demoHandlerV1 struct{}
+
+func (h *demoHandlerV1) uploadDemo(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
 	if err := r.ParseMultipartForm(10 >> 20); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		writeHttpError(w, http.StatusBadRequest, err)
 		return
 	}
 
 	f, fh, err := r.FormFile("demo")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	defer f.Close()
@@ -25,13 +53,20 @@ func uploadDemoHandlerV1(w http.ResponseWriter, r *http.Request) {
 
 	parser, err := demoparser.New(f, fh)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		writeHttpError(w, http.StatusBadRequest, err)
 		return
 	}
 	defer parser.Close()
 
 	if err := parser.Parse(); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		writeHttpError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(uploadDemoRes{
+		DemoJobID: uuid.New(), // TODO: return REAL demo job id
+	}); err != nil {
+		writeHttpError(w, http.StatusInternalServerError, err)
 		return
 	}
 
@@ -43,6 +78,9 @@ func main() {
 		AddSource: false,
 		Level:     slog.LevelInfo,
 	})))
-	http.HandleFunc("/v1/demos", uploadDemoHandlerV1)
+
+	demoHandlerV1 := &demoHandlerV1{}
+
+	http.HandleFunc("/v1/demos", demoHandlerV1.uploadDemo)
 	http.ListenAndServe(":8080", nil)
 }
