18507d75f539c30d94cc4251dfeda255551e9fd2
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2023-12-02 11:00:47 +0400

    ref: remove examples from writer pkg

--- server/internal/httpapi/writer/error.go
+++ server/internal/httpapi/writer/error.go
@@ -3,25 +3,6 @@
 // The primary purpose of this package is to simplify the process of responding to HTTP requests with JSON-encoded error messages.
 // It includes functions to generate JSON responses for different scenarios, such as returning custom messages or standard HTTP status codes.
 //
-// Example usage:
-//
-//	// Respond with a custom error message
-//	err := writer.Message(w, http.StatusBadRequest, "Invalid input data")
-//
-//	// Respond with a standard HTTP status code and its corresponding message
-//	err := writer.Status(w, http.StatusOK)
-//
-//	// Respond with an error object and its corresponding status code
-//	err := writer.Error(w, http.StatusInternalServerError, someError)
-//
-// JSON Format:
-//
-//	The responses are formatted as JSON objects with the following structure:
-//	{
-//	  "code": "HTTP Status Code or Custom Code",
-//	  "msg": "Error Message or Standard HTTP Status Text"
-//	}
-//
 // Package Naming:
 //
 //	To avoid naming conflicts with the standard "http" package, this package is named "writer."
